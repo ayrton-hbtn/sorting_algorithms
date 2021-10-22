89c1aa4dd41ee7197b4f050c4609ad9cffc406d9
Ayrton Coelho <3282@holbertonschool.com>
2021-10-22 13:33:51 -0300

    bubble sort: first try

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,18 +8,25 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-    int i, tmp;
+    int i, tmp, flag = 1;
 
-    for (i = 0; array[i]; i++)
+    while (flag)
     {
-        if (array[i + 1] && (array[i] > array[i + 1]))
+        for (i = 0; array[i]; i++)
         {
-            tmp = array[i + 1];
-            array[i + 1] = array[i];
-            array[i] = tmp;
-            print_array(array, size);
+            flag = 1;
+            if (array[i + 1] && (array[i] > array[i + 1]))
+            {
+                tmp = array[i + 1];
+                array[i + 1] = array[i];
+                array[i] = tmp;
+                print_array(array, size);
+                flag++;
+            }
+            else
+                continue;
         }
-        else
-            continue;
+        if (flag == 1)
+            flag = 0;
     }
 }
