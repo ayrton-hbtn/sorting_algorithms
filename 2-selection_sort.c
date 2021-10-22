0b53bcc4993a3f545a64f56f48a0f1b9cff65dbd
hippocampus3282 <3282@holbertonschool.com>
2021-10-22 14:57:09 -0700

    selection: 2nd try

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,12 +7,13 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    int i, j, position, tmp;
+    size_t i, j, position;
+	int tmp;
 
-    for (i = 0; i < size - 1, i++)
+    for (i = 0; i < size - 1; i++)
     {
         position = i;
-        for (j = i + 1; j < n; j++)
+        for (j = i + 1; j < size; j++)
         {
             if (array[position] > array[j])
                 position = j;
