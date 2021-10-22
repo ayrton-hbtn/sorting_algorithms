d1f3903d0dffbc54ab3c6091aa3877a3829d9597
Ayrton Coelho <3282@holbertonschool.com>
2021-10-22 15:41:01 -0300

    insertion_sort: first try

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+*
+*
+*
+*
+*/
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *tmp = *list, *after, *swap;
+
+    while (tmp->next)
+    {
+        after = tmp->next;
+        while (after->prev && after->prev->n > after->n)
+        {
+            swap = after->prev;
+            if (swap->prev)
+            {
+                swap->prev->next = after;
+                after->perv = swap->prev;
+            }
+            if (after->next)
+            {
+                after->next->prev = swap;
+                swap->next = after->next;
+            }
+            swap->prev = after;
+            after->next = swap;
+        }
+        print_list(*list);
+        tmp = tmp->next;
+    }
+}
