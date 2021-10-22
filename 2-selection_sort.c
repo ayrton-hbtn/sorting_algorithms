dc74fb97fac020e589a8f4a79870e7a2306bdc8b
Ayrton Coelho <3282@holbertonschool.com>
2021-10-22 18:48:19 -0300

    selection: first try

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,10 +7,22 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    int i, tmp = array[0];
+    int i, j, position, tmp;
 
-    for (i = 0; array[i], i++)
+    for (i = 0; i < size - 1, i++)
     {
-        
+        position = i;
+        for (j = i + 1; j < n; j++)
+        {
+            if (array[position] > array[j])
+                position = j;
+        }
+        if (position != i)
+        {
+            tmp = array[i];
+            array[i] = array[position];
+            array[position] = tmp;
+        }
+        print_array(array, size);
     }
 }
