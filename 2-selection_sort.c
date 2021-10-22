5f023c7ee82f567b339c525e5e2f8ec2ecd37381
Ayrton Coelho <3282@holbertonschool.com>
2021-10-22 18:27:30 -0300

    2

---
+++ 2-selection_sort.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+
+/**
+ * 
+ * 
+ * 
+ */
+void selection_sort(int *array, size_t size)
+{
+    int i, tmp = array[0];
+
+    for (i = 0; array[i], i++)
+    {
+        
+    }
+}
