ecb37a2c04a1f93a2c6c6908d081c72a2e1e308e
hippocampus3282 <3282@holbertonschool.com>
2021-10-23 14:58:26 -0700

    quick_sort: first try

---
+++ 3-quick_sort.c
@@ -0,0 +1,76 @@
+#include "sort.h"
+
+/**
+  * swap - swaps two numbers in an array
+  * @a: pointer to first number
+  * @b: pointer to second number
+  */
+void swap(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+  * partition - sets last element of array as pivot
+  * and first element is maintained while another
+  * pointer is scanning through the array. The scan
+  * ensures that any element from start to end is less
+  * than the pivot element in value and elements from
+  * fixed ptr to scanning ptr are bigger than or equal
+  * to pivot element (Lomuto schema)
+  * @array: pointer to array of int
+  * @low: starting index
+  * @high: last index
+  * @size: size of array
+  * Return: index of pivot
+  */
+int partition(int *array, int low, int high, size_t size)
+{
+	int i = (low - 1), pivot = array[high], j;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			swap(&array[i], &array[j]);
+		}
+	}
+	swap(&array[i + 1], &array[high]);
+	print_array(array, size);
+	return (i + 1);
+}
+
+/**
+  * quicksort - implement the Lomuto partition scheme
+  * for the quick sort algorithm
+  * @array: pointer to first element of array
+  * @low: first element of array to sort
+  * @high: last element of array to sort
+  * @size: size of array
+  */
+void quicksort(int *array, int low, int high, size_t size)
+{
+	int pivot;
+
+	if (low < high)
+	{
+		pivot = partition(array, low, high, size);
+		quicksort(array, low, pivot - 1, size);
+		quicksort(array, pivot + 1, high, size);
+	}
+}
+
+/**
+  * quick_sort - implements the quick sort algorithm
+  * with Lomuto schema
+  * @array: pointer to first element of unsorted array
+  * @size: size of array
+  */
+void quick_sort(int *array, size_t size)
+{
+	quicksort(array, 0, size - 1, size);
+}
